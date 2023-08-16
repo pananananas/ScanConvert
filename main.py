39139fc7cf4381764e4f079289915754f763f7ac
pananananas <101266606+pananananas@users.noreply.github.com>
2023-08-16 22:30:07 +0200

    changed a few variables for more univeral use

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import os
 
 
-
 def rotate_and_crop_image(image, angle):
     # Oblicz środek obrazu
     h, w = image.shape[:2]
@@ -41,26 +40,22 @@ def extract_images_from_scan(scan_path, output_dir, total_count):
 
     for index, c in enumerate(contours):
         x, y, w, h = cv2.boundingRect(c)
-        if w > 250 and h > 250 and w < 2000 and h < 2000:
+        if w > 700 and h > 700 and w < 3000 and h < 3000:
             # Oblicz kąt obrotu dla konturu
             _, _, angle = cv2.minAreaRect(c)
 
-            # Jeśli kąt jest większy niż 45 stopni, przekształć go
-            # (zakładając, że chcemy mały kąt obrotu)
             if angle > 45:
                 angle -= 90
 
-            # Wycięcie regionu zainteresowania
             roi = image[y:y+h, x:x+w]
 
-            # Obróć wycięty obraz
             rotated_roi = rotate_and_crop_image(roi, angle)
 
             cv2.imwrite(os.path.join(output_dir, f'image_{total_count}.png'), rotated_roi)
             total_count += 1
             img_count += 1
 
-    if img_count < 3:
+    if img_count != 3:
         print(f"Error extracting images from {scan_path} - found {img_count} images")
         cv2.imshow("Input", image)
         cv2.waitKey(0)
@@ -71,8 +66,8 @@ def extract_images_from_scan(scan_path, output_dir, total_count):
 
 
 def main():
-    input_dir = "Scans/Album1"
-    output_dir = "Images/Album1"
+    input_dir = "Scans/Album2"
+    output_dir = "Images/Album2"
 
     if os.path.exists(output_dir):                                          # delete all contents of output_dir if it exists
         for filename in os.listdir(output_dir):
@@ -90,7 +85,7 @@ def main():
     total_count = 0
     no_input_imgs = len(os.listdir(input_dir))
 
-    for filename in os.listdir(input_dir):
+    for filename in sorted(os.listdir(input_dir)):
         if filename.endswith(".png"):
             count, img_extraction_error = extract_images_from_scan(os.path.join(input_dir, filename), output_dir, total_count)
             ind += 1
