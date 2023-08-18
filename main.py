3bfe057483be1bc6db41a8c6976541b418a9b752
pananananas <101266606+pananananas@users.noreply.github.com>
2023-08-18 12:41:44 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Initial parameters
+
+min_image_width  = 700
+min_image_height = 700
+max_image_width  = 3000
+max_image_height = 3000
+
+number_of_images_per_scan = 3
+
+input_dir = "Scans/Album2"
+output_dir = "Images/Album2"
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # 
+
 import cv2
 import os
 
 
 def rotate_and_crop_image(image, angle):
-    # Oblicz środek obrazu
     h, w = image.shape[:2]
-    center = (w / 2, h / 2)
+    img_center = (w / 2, h / 2)
 
-    # Utwórz macierz obrotu
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    M = cv2.getRotationMatrix2D(img_center, angle, 1.0)
 
-    # Obróć obraz
     rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
 
-    # Przytnij białe marginesy
     gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
     _, thresholded = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
-        c = max(contours, key=cv2.contourArea)  # znajdź największy kontur
+        c = max(contours, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(c)
         rotated_and_cropped = rotated[y:y+h, x:x+w]
         return rotated_and_cropped
@@ -27,47 +38,43 @@ def rotate_and_crop_image(image, angle):
         return rotated
 
 
-def extract_images_from_scan(scan_path, output_dir, total_count):
+def extract_images_from_scan(scan_path, total_count):
     
     img_count = 0
     img_extraction_error = False
 
     image = cv2.imread(scan_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresholded = cv2.threshold(gray, 245, 255, cv2.THRESH_BINARY_INV)
-
-    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _    = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for index, c in enumerate(contours):
+
         x, y, w, h = cv2.boundingRect(c)
-        if w > 700 and h > 700 and w < 3000 and h < 3000:
-            # Oblicz kąt obrotu dla konturu
+        if w > min_image_width and h > min_image_height and w < max_image_width and h < max_image_height:
             _, _, angle = cv2.minAreaRect(c)
 
             if angle > 45:
                 angle -= 90
-
             roi = image[y:y+h, x:x+w]
-
+            
             rotated_roi = rotate_and_crop_image(roi, angle)
 
             cv2.imwrite(os.path.join(output_dir, f'image_{total_count}.png'), rotated_roi)
             total_count += 1
             img_count += 1
 
-    if img_count != 3:
+    if img_count != number_of_images_per_scan:
         print(f"Error extracting images from {scan_path} - found {img_count} images")
         cv2.imshow("Input", image)
         cv2.waitKey(0)
+        cv2.destroyAllWindows()
         img_extraction_error = True
 
     return img_count, img_extraction_error
 
 
-
 def main():
-    input_dir = "Scans/Album2"
-    output_dir = "Images/Album2"
 
     if os.path.exists(output_dir):                                          # delete all contents of output_dir if it exists
         for filename in os.listdir(output_dir):
@@ -81,21 +88,23 @@ def main():
         os.makedirs(output_dir)
 
     ind = 0
-    no_errors = 0
+    num_of_errors = 0
     total_count = 0
-    no_input_imgs = len(os.listdir(input_dir))
+    num_of_input_imgs = len(os.listdir(input_dir))
 
     for filename in sorted(os.listdir(input_dir)):
         if filename.endswith(".png"):
-            count, img_extraction_error = extract_images_from_scan(os.path.join(input_dir, filename), output_dir, total_count)
+            count, img_extraction_error = extract_images_from_scan(os.path.join(input_dir, filename), total_count)
             ind += 1
-            print(f"Processed {ind} out of {no_input_imgs}, {filename} ")
+            print(f"Processed {ind} out of {num_of_input_imgs}, {filename} ")
             total_count += count
             if img_extraction_error:
-                no_errors += 1
+                num_of_errors += 1
+                
+    num_of_output_imgs = len(os.listdir(output_dir))
+    print (f"Total images extracted: {num_of_output_imgs}, expected: {num_of_input_imgs * number_of_images_per_scan}")
+    print (f"Total errors: {num_of_errors}")
 
-    print (f"Total images extracted: {total_count}, expected: {no_input_imgs * 3}")
-    print (f"Total errors: {no_errors}")
 
 if __name__ == "__main__":
     main()
